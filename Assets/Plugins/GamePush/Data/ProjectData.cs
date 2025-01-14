513362d0a5c4c05b4fdfc4936a9afddf4f01efd9
Vitalich <v7i7t71@yandex.ru>
2025-01-14 21:59:55 +0300

    Add
    Have been added GamePush SDK

---
+++ Assets/Plugins/GamePush/Data/ProjectData.cs
@@ -0,0 +1,10 @@
+namespace GamePush.Data
+{
+    public static class ProjectData
+    {
+        public static string SDK_VERSION = "1.6.1";
+        public static string ID = "19722";
+        public static string TOKEN = "bwXNFiY6ahr88B11jpQx0BnFx0y394LT";
+        public static bool GAMEREADY_AUTOCALL = true;
+    }
+}
---
+++ Assets/Plugins/GamePush/JS/jspreData.txt
@@ -0,0 +1,4 @@
+const dataProjectId = '19722';
+const dataPublicToken = 'bwXNFiY6ahr88B11jpQx0BnFx0y394LT';
+const showPreloaderAd = 'False';
+const autocallGameReady = 'True';
