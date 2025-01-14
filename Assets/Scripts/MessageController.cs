4559347174f870f8793b6e796f4c77c13000564b
Vitalich <v7i7t71@yandex.ru>
2025-01-14 12:33:49 +0300

    Commit
    Improvements

--- Assets/Scripts/MessageController.cs
+++ Assets/Scripts/MessageController.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MessageController : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI messageTextField;
+    [SerializeField] string messageText;
+
+    public void MessageInit()
+    {
+        messageTextField.text = messageText;
+    }
+
     public void MessageAnimationEnd()
     {
         gameObject.SetActive(false);
